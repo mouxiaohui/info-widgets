e55bc77f63cf50c3faf14327554a9103817ac7c0
mouxiaohui <18367652239@163.com>
2022-04-25 22:26:13 +0800

    initialize

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="app" data-tauri-drag-region>Hello World</div>
+</template>
+
+<style>
+html,
+body,
+#app {
+  padding: 0;
+  margin: 0;
+  width: 100%;
+  height: 100%;
+}
+
+.app {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+</style>
